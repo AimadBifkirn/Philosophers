9f0dff2f193548dcf1c8e08ea2c8fb1a506cf1c4
AimadBifkirn <aimadbifkirn@gmail.com>
2025-05-13 19:27:33 +0100

    trying to understand the problem and find the solution, offcourse i am reading about threads and mutex

---
+++ philo/main.c
@@ -0,0 +1,64 @@
+#include "philosophers.h"
+
+t_infos	init_struct(char **argv)
+{
+	t_infos	res;
+
+	res.n_philos = ft_atoi(argv[1]);
+	res.time_to_d = ft_atoi(argv[2]);
+	res.time_to_e = ft_atoi(argv[3]);
+	res.time_to_s = ft_atoi(argv[4]);
+	if (argv[5])
+		res.num_times_eat = ft_atoi(argv[5]);
+	else
+		res.num_times_eat = -1;
+	return (res);
+}
+
+void	*handel_threads(void *i)
+{
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	printf ("%ld\t", time.tv_sec);
+	printf ("this is philosofer %d\n", *((int *)i) + 1);
+	free (i);
+	return (NULL);
+}
+
+void	create_threads(t_infos *infos)
+{
+	int	i;
+	static pthread_t	*pids;
+
+	i = 0;
+	pids = malloc(sizeof(pthread_t) * infos->n_philos);
+	pthread_mutex_t		zz;
+	pthread_mutex_init(&zz, NULL);
+	while (i < infos->n_philos)
+	{
+		int *arg = malloc(sizeof(int));
+		*arg = i;
+		pthread_create(&pids[i], NULL, handel_threads, arg);
+		i++;
+	}
+	i = 0;
+	while (i < infos->n_philos)
+	{
+		pthread_join(pids[i], NULL);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	t_infos	infos;
+
+	if (argc < 5 || argc > 6)
+	{
+		write (2, "invalid number of arguments\n", 29);
+		return (1);
+	}
+	infos = init_struct(argv);
+	create_threads(&infos);
+	return (0);
+}
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 typedef struct s_infos
 {
